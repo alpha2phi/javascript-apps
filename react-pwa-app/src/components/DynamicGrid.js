db8dc0e3087c86414b56265e83b05a3e7032349a
Meng Wang <mengwangk@gmail.com>
2021-04-25 15:09:48 +0800

    Initial commit

---
+++ react-pwa-app/src/components/DynamicGrid.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from 'react';
+import './DynamicGrid.css';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import { AgGridReact } from 'ag-grid-react';
+import faker from "faker";
+import { CountryRenderer, SubcountryRenderer, CityRenderer } from "./LookupCity";
+
+export default function DynamicGrid() {
+	const columnDefs = [{
+		field: "row_id",
+		headerName: "#",
+		checkboxSelection: function(params) {
+			if (params.data.changed) {
+				return true;
+			}
+			return false;
+		},
+		width: 50,
+		headerCheckboxSelection: true,
+		pinned: 'left'
+	},
+	{ field: "name", headerName: "Name", filter: true, sortable: true, resizable: true, floatingFilter: true, pinned: 'left' },
+	{ field: "email", headerName: "Email", filter: true, sortable: true, resizable: true, floatingFilter: true },
+	{
+		field: "country",
+		headerName: "Country",
+		resizable: true,
+		cellRenderer: "CountryRenderer",
+		width: 350
+	},
+	{
+		field: "subcountry",
+		headerName: "Subcountry",
+		resizable: true,
+		cellRenderer: "SubcountryRenderer",
+		width: 350
+	},
+	{
+		field: "city",
+		headerName: "City",
+		resizable: true,
+		cellRenderer: "CityRenderer",
+		width: 350
+	},
+	];
+
+	const [data, setData] = useState([]);
+	const [gridApi, setGridApi] = useState(null);
+	const frameworkComponents = {
+		CountryRenderer: CountryRenderer,
+		SubcountryRenderer: SubcountryRenderer,
+		CityRenderer: CityRenderer,
+	};
+
+	useEffect(() => {
+		async function loadData() {
+			const persons = [];
+			for (var i = 0; i < 1000; i++) {
+				var person = {};
+				person.name = faker.name.findName();
+				person.email = faker.internet.email();
+				person.country = "";
+				person.subcountry = "";
+				person.city = "";
+				persons.push(person);
+			}
+			setData(persons);
+
+		}
+		loadData();
+	}, []);
+
+	function onGridReady(params) {
+		setGridApi(params.api);
+	}
+
+	const onPageSizeChanged = () => {
+		const value = document.getElementById('page_size').value;
+		gridApi.paginationSetPageSize(Number(value));
+	};
+
+	return (
+		<div className="grid-header">
+			<h1>Dynamic Grid</h1>
+			<div className="paging">
+				Page Size: &nbsp;
+				<select onChange={() => onPageSizeChanged()} id="page_size">
+					<option value="10" defaultValue={true}>10</option>
+					<option value="20">20</option>
+					<option value="50">50</option>
+					<option value="100">100</option>
+					<option value="500">500</option>
+				</select>
+			</div>
+			<div className="ag-theme-alpine" style={{
+				height: 600, marginLeft: 20, marginRight: 20, marginTop: 10
+			}}>
+				<AgGridReact
+					onGridReady={onGridReady}
+					rowData={data}
+					columnDefs={columnDefs}
+					suppressRowClickSelection={true}
+					pagination={true}
+					paginationPageSize={10}
+					enableRangeSelection={true}
+					paginationNumberFormatter={
+						function(params) { return '[' + params.value.toLocaleString() + ']'; }}
+					rowSelection={'multiple'}
+					sizeColumnsToFit={true}
+					enableCellTextSelection={true}
+					frameworkComponents={frameworkComponents}
+					isRowSelectable={
+						function(rowNode) { return false; }}
+				/>
+			</div>
+		</div>
+	);
+}
---
+++ react-pwa-app/src/components/LookupCity.js
@@ -0,0 +1,127 @@
+import React from 'react';
+
+const cities = require('../data/cities.json');
+const LOOKUP = {};
+const COUNTRIES = [];
+
+cities.forEach(function(city) {
+
+	// Country to subcountries
+	if (LOOKUP[city.country]) {
+		const subcountries = LOOKUP[city.country];
+		if (!subcountries.includes(city.subcountry)) {
+			subcountries.push(city.subcountry);
+		}
+	} else {
+		COUNTRIES.push(city.country);
+		const subcountries = [];
+		subcountries.push(city.subcountry);
+		LOOKUP[city.country] = subcountries;
+	}
+
+	// Subcountry to cities
+	if (LOOKUP[city.subcountry]) {
+		const cities = LOOKUP[city.subcountry];
+		if (!cities.includes(city.name)) {
+			cities.push(city.name);
+		}
+	} else {
+		const cities = [];
+		cities.push(city.name);
+		LOOKUP[city.subcountry] = cities;
+	}
+});
+
+COUNTRIES.sort();
+
+Object.keys(LOOKUP).forEach(function(key) {
+	const values = LOOKUP[key];
+	values.sort();
+});
+
+
+function setDirty(props) {
+    if (props.data.changed) return;
+
+    props.data.changed = true;
+    const rows = [];
+    rows.push(props.node);
+    props.node.selectable = true;
+    props.api.redrawRows({ rowNodes: rows });
+}
+
+export function CountryRenderer(props) {
+
+	const onChangeCountry = (e) => {
+		setDirty(props);
+
+		props.data.country = e.target.value;
+		props.data.subcountry = "";
+		props.data.city = "";
+		props.api.refreshCells({ columns: ['subcountry', 'city'], force: true });
+	};
+
+	return (
+		<div>
+			<select defaultValue={props.data.country} onChange={(e) => onChangeCountry(e)}>
+				<option key=""></option>
+				{COUNTRIES.map(country =>
+					<option key={country}>{country}</option>
+				)}
+			</select>
+		</div>
+	)
+}
+
+export function SubcountryRenderer(props) {
+	var country = props.data.country;
+	var subcountries = [];
+	if (country) {
+		subcountries = LOOKUP[country];
+	}
+
+	const onChangeSubcountry = (e) => {
+		setDirty(props);
+
+		props.data.subcountry = e.target.value;
+		props.data.city = "";
+		props.api.refreshCells({ columns: ['city'], force: true });
+	};
+
+	return (
+		<div>
+			<select defaultValue={props.data.subcountry} onChange={(e) => onChangeSubcountry(e)}>
+				<option key=""></option>
+				{subcountries.map(subcountry =>
+					<option key={subcountry}>{subcountry}</option>
+				)}
+			</select>
+		</div>
+	)
+}
+
+export function CityRenderer(props)  {
+	var subcountry = props.data.subcountry;
+	var cities = [];
+	if (subcountry) {
+		cities = LOOKUP[subcountry];
+		if (!cities) cities = [];
+	}
+
+	const onChangeCity = (e) => {
+		setDirty(props);
+
+		props.data.city = e.target.value;
+	};
+
+	return (
+		<div>
+			<select defaultValue={props.data.city} onChange={(e) => onChangeCity(e)}>
+				<option key=""></option>
+				{cities.map(city =>
+					<option key={city}>{city}</option>
+				)}
+			</select>
+		</div>
+	)
+}
